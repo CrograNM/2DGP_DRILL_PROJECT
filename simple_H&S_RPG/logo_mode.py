5dca4da5db8b40a28a0f8b96242f9c31923db81d
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-11-14 16:30:52 +0900

    logo_mode 추가

---
+++ simple_H&S_RPG/logo_mode.py
@@ -0,0 +1,28 @@
+from pico2d import load_image, get_time, clear_canvas, update_canvas, get_events
+import game_framework
+import play_mode
+
+def init():
+    global image
+    global logo_start_time
+
+    image = load_image('tuk_credit.png')
+    logo_start_time = get_time()
+
+def finish():
+    global image
+    del image # image는 용량이 크기 때문에 지워주도록 한다.
+
+def update():
+    global logo_start_time
+    if get_time() - logo_start_time >= 2.0:
+        logo_start_time = get_time()
+        game_framework.change_mode(play_mode)
+
+def draw():
+    clear_canvas()
+    image.draw(400,300)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
