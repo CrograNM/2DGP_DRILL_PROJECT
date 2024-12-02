0a9292bf18efaab2d3c366e057e764048af330c0
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-12-03 00:31:54 +0900

    게임 시작 모드 변경

--- simple_H&S_RPG/main.py
+++ simple_H&S_RPG/main.py
@@ -1,8 +1,8 @@
 import pico2d
 import game_framework
 #import play_mode as start_mode
-#import logo_mode as start_mode
-import title_mode as start_mode
+#import title_mode as start_mode
+import logo_mode as start_mode
 
 pico2d.open_canvas(1280, 720)
 game_framework.run(start_mode)
